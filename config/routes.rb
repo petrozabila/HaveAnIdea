a14c347c94ba23c7f93c2231e1d3e39009646783
Artem Komarov <artyemka@gmail.com>
2015-01-29 20:52:28 +0200

    install active_admin and exlude active_admin styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get 'sessions/login' => 'sessions#login', as: 'sessions_login'
  
   get 'sessions/logout' => 'sessions#logout', as: 'sessions_logout'
