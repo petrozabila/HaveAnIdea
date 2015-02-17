9696f10a0434b357e2cd0fe4d158b0f6da76bddf
Artem Komarov <artyemka@gmail.com>
2015-02-17 20:01:15 +0200

    gravatar is?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
   def avatar_url(user)
   	gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-  	"http://gravatar.com/avatar/#{gravatar_id}.png?s=50"
+    if gravatar_id
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=50"
+    end
   end
 end
